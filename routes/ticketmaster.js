2e6446aa78c77fd266449fdeea8c8b2f63c8a3eb
Philip Aguiar <PhilipAguiar@gmail.com>
2022-03-30 13:31:22 -0400

    created event paths by venue

--- routes/ticketmaster.js
+++ routes/ticketmaster.js
@@ -8,11 +8,25 @@ const API_KEY = "ITWvRMBQUpQuDn6octJPGuaWxlrwOcxT"
 
 router.get('/', (req, res) => {
   
-  https://app.ticketmaster.com/discovery/v2/events.json?latlong=43.653225,-79.3831861&apikey=ITWvRMBQUpQuDn6octJPGuaWxlrwOcxT
-  axios.get(`${BASE_URL}discovery/v2/events.json?latlong=${43.653225},${-79.3831861}&apikey=${API_KEY}`).then((axiosRes)=>{
-   
+  https://app.ticketmaster.com/discovery/v2/suggest?apikey=ITWvRMBQUpQuDn6octJPGuaWxlrwOcxT&latlong=43.653225,-79.3831861&radius=100
+
+  // axios.get(`${BASE_URL}discovery/v2/events.json?latlong=${43.653225},${-79.3831861}&apikey=${API_KEY}`).then((axiosRes)=>{
+   axios.get(`${BASE_URL}discovery/v2/suggest?latlong=${43.653225},${-79.3831861}&apikey=${API_KEY}&radius=100`).then((axiosRes)=>{
     
-    res.status(200).json(axiosRes.data._embedded.events[0]);
+    res.status(200).json(axiosRes.data._embedded.venues)
+  })
+});
+
+
+router.get('/:id', (req, res) => {
+  https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=ITWvRMBQUpQuDn6octJPGuaWxlrwOcxT&venueId=KovZpZAFlaAA
+
+
+  // axios.get(`${BASE_URL}discovery/v2/events.json?latlong=${43.653225},${-79.3831861}&apikey=${API_KEY}`).then((axiosRes)=>{
+    
+   axios.get(`${BASE_URL}discovery/v2/events.json?size=5&apikey=${API_KEY}&venueId=${req.params.id}`).then((axiosRes)=>{
+    
+    res.status(200).json(axiosRes.data)
   })
 });
 
