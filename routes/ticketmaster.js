0a22d4ac850f67358968c8483ddef037a00c314f
Philip Aguiar <PhilipAguiar@gmail.com>
2022-03-31 17:23:38 -0400

    code save

--- routes/ticketmaster.js
+++ routes/ticketmaster.js
@@ -8,11 +8,11 @@ const API_KEY = "ITWvRMBQUpQuDn6octJPGuaWxlrwOcxT"
 
 router.get('/', (req, res) => {
   
-  https://app.ticketmaster.com/discovery/v2/suggest?apikey=ITWvRMBQUpQuDn6octJPGuaWxlrwOcxT&latlong=43.653225,-79.3831861&radius=100
-
-  // axios.get(`${BASE_URL}discovery/v2/events.json?latlong=${43.653225},${-79.3831861}&apikey=${API_KEY}`).then((axiosRes)=>{
-   axios.get(`${BASE_URL}discovery/v2/suggest?latlong=${43.653225},${-79.3831861}&apikey=${API_KEY}&radius=100`).then((axiosRes)=>{
-    
+  let lat = req.query.lat
+  let lng = req.query.lng
+  console.log(lat)
+   axios.get(`${BASE_URL}discovery/v2/suggest?latlong=${lat},${lng}&apikey=${API_KEY}&size=5&radius=100`).then((axiosRes)=>{
+    console.log(axiosRes.data._embedded.venues)
     res.status(200).json(axiosRes.data._embedded.venues)
   })
 });
