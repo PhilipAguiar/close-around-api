94c2ab09eecde6093a5393256d200c330733ea76
Philip Aguiar <PhilipAguiar@gmail.com>
2022-04-21 21:31:22 -0400

    cors bigfix test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static('public'))
 app.use(express.json())
 app.use(cors());
-
+app.options('*', cors())
 // Routes
 const ticketmasterRoute = require('./routes/ticketmaster');
 app.use('/ticketmaster', ticketmasterRoute);
